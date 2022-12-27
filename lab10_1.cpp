ad940092a61c9e5e1c1b8763a7e1e6c02b1910fe
SUPATKN <yacht1400@gmail.com>
2022-12-27 15:56:53 +0700

    lab10_1 c

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double Money, Perc, payback,Prevba;
 	cout << "Enter initial loan: ";
+	cin >> Money;
 	cout << "Enter interest rate per year (%): ";
+	cin >> Perc;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payback;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,26 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	Prevba = Money;
+	for(int i=1;1;i++){
+		double Interest = Prevba*(Perc/100), Total = Prevba + Interest, NewBa = Total-payback;
+		if(NewBa<=0){
+			payback = Total;
+			NewBa = 0;
+		}
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << Prevba;
+		cout << setw(13) << left << Interest;
+		cout << setw(13) << left << Total;
+		cout << setw(13) << left << payback;
+		cout << setw(13) << left << NewBa;
+		cout << "\n";
+		Prevba = NewBa;
+		if(NewBa==0){
+			break;
+		}
+	}	
 	
 	return 0;
 }
